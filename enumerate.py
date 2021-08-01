6cf4031726b9d23d52383161b27a1cd06ce88d9d
pizza-power <pizzapwr@gmail.com>
2021-07-31 20:48:03 -0400

    add banner, add colorama, add more error handling

--- enumerate.py
+++ enumerate.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
-
-# bug bounty enumeration version 3
+#
+# Title:enuMM
+#
+# Author: pizzapower - Matthew Moreschi (where the MM comes from)
+#
+# A bug bounty enumeration script.
+#
 # basic operations
 # pizza-enum.py --header "reasearcher: pizzapower" --threads 2 --input-file domains.txt --blacklist blacklist.txt
 #
 # or for a single domain
 # pizza-enum.py --domain pizzapower.me
 #
-#
-#
-# MVP - runs amass, screenshots webservers, keeps running and notifies
+# MVP == Refactor
+# Pass argparse object to each function then just run them as you want
+# run_amass()
 
 
 import argparse
@@ -17,9 +22,34 @@ import sys
 import os
 import subprocess
 from shutil import which
+from colorama import init, Fore, Style
+import pyfiglet
 
-# used when running programs that can use multiple threads
-# THREADS = 1
+init(autoreset=True)
+
+
+def printBanner():
+
+    banner = pyfiglet.figlet_format("enuMMv0.2")
+    print(banner)
+    return
+
+
+def pos_print(text: str):
+    """
+    :param text: function to print a green plus sign in brackets if the outcome
+    of whatever code is favorable
+    """
+    print("\n" + Fore.GREEN + "[+] " + Style.RESET_ALL + text)
+
+
+def neg_print(text: str):
+    """
+    :param text: function to print a red minus sign in brackets if the outcome
+    of whatever code is not favorable
+    """
+    print("\n" + Fore.RED + "[-] " + Style.RESET_ALL + text)
+
 
 # parse the CLI arguments
 def parse_args():
@@ -43,7 +73,9 @@ def parse_args():
     )
 
     # blacklist
-    parser.add_argument("-b", "--blacklist", help="a list of domains to ignore")
+    parser.add_argument(
+        "-b", "--blacklist", help="a list of domains to ignore"
+    )
 
     # output file
     parser.add_argument(
@@ -74,16 +106,20 @@ def check_dependencies(dependency_list: list):
     do_they_exist = [
         program for program in dependency_list if which(program) is not None
     ]
-    shit_you_dont_have_installed = list(set(dependency_list) ^ set(do_they_exist))
+    shit_you_dont_have_installed = list(
+        set(dependency_list) ^ set(do_they_exist)
+    )
     if shit_you_dont_have_installed:
-        print(
+        neg_print(
             "The following programs are not in $PATH. Please install them: "
             + str(shit_you_dont_have_installed)
         )
+        sys.exit(-1)
     return
 
 
 def run_amass(domain: str):
+    pos_print(f"Running amass on: {domain}")
     try:
         filename = f"{domain}/amass-{domain}.txt"
         subprocess.call(
@@ -101,7 +137,7 @@ def run_amass(domain: str):
             ]
         )
     except Exception as e:
-        print(e)
+        neg_print(e)
     return
 
 
@@ -120,20 +156,21 @@ def run_eyewitness(domain: str):
     try:
         os.system(command)
     except Exception as e:
-        print(e)
+        neg_print(e)
     return
 
 
 def run_httpx(domain: str):
+    pos_print(f"Running httpx on: {domain}")
     # httpx -ports 80,443,8009,8080,8081,8090,8180,8443 -l subdomains.txt -o httpx.txt
     # create subdomains from amass list
+    # can maybe do this with just amass and db flag or something
     command = f'cut -d " " -f 2 {domain}/amass-{domain}.txt | tr , "\n" | uniq > {domain}/ips-{domain}.txt'
-    print("running httpx...")
     try:
         os.system(command)
 
     except Exception as e:
-        print(e)
+        neg_print(e)
 
     try:
         input_filename = f"{domain}/ips-{domain}.txt"
@@ -151,7 +188,7 @@ def run_httpx(domain: str):
             ]
         )
     except Exception as e:
-        print(e)
+        neg_print(e)
     return
 
 
@@ -169,7 +206,10 @@ def find_new_subdomains(domain: str):
 
 
 def get_ip_addresses_from_subdomain_list():
-    """uses amass ip to get ips from already run scan"""
+    """uses amass ip to get ips from already run scan
+    won't need this is we use amass db command
+    """
+
     """
     cut -d " " -f 2 amass-westernunion.com.txt | tr , '\n' | uniq
     """
@@ -186,26 +226,29 @@ def get_domains_from_amass_output():
 
 
 def main():
+    printBanner()
     # check dependencies to make sure you have the proper stuff installed
-    dependency_list = ["amass", "httpx"]
+    # dependency_list = ["amass", "httpx"]
+    dependency_list = ["amass"]
+
     check_dependencies(dependency_list)
 
     # get the command line arguments
     # eyewitness just passes this to functions
     arguments = parse_args()
-    # print(arguments)
-    # path = os.getcwd()
-    # print(path)
-    # print(os.path.join(os.getcwd(), arguments.domains))
 
     # get the blacklisted domains
     # amass actually has a blacklist option
     # amass enum -blf data/blacklist.txt -d example.com
+
     if arguments.blacklist:
-        with open(arguments.blacklist) as blacklist:
-            blacklisted = [line.strip() for line in blacklist]
-            # print(blacklisted)
-            blacklist.close()
+        try:
+            with open(arguments.blacklist) as blacklist:
+                blacklisted = [line.strip() for line in blacklist]
+                blacklist.close()
+        except FileNotFoundError as e:
+            neg_print("Blacklist not found in this directory. Exiting!")
+            sys.exit(-1)
 
     # run on a single domain
     if arguments.domain:
@@ -216,9 +259,13 @@ def main():
 
     # run on a list of domains
     if arguments.domains:
-
-        with open(arguments.domains) as domains:
-            domain_list = [line.strip() for line in domains]
+        try:
+            with open(arguments.domains) as domains:
+                domain_list = [line.strip() for line in domains]
+            domains.close()
+        except FileNotFoundError as e:
+            neg_print("Whitelist not found in this directory. Exiting!")
+            sys.exit(-1)
 
         # need to compare white and black list
         if arguments.blacklist:
@@ -230,17 +277,11 @@ def main():
 
         # thread this
         for domain in final_domains:
-            # make a subdirectory for the domain
+            # mkdir then run apps
             os.mkdir(domain)
-
-            # run amass on the domain
             run_amass(domain)
             run_httpx(domain)
             run_eyewitness(domain)
-
-        domains.close()
-
-        sys.exit()
 
     sys.exit()
 
